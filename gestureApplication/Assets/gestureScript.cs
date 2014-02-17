c5b3d60169a31fd3c778b7f4b0b06d1fbf55ca56
Tara Siegel <Tara@vagvlan536.1171.wlan.wireless-pennnet.upenn.edu>
2014-02-16 20:05:46 -0500

    trying to add sample gesture app

---
+++ gestureApplication/Assets/gestureScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gestureScript : gestureBase {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTap( TapGesture gesture ) 
+	{
+		UI.StatusText = "Tapped with finger " + gesture.Fingers[0] + " at: " + gesture.Position;
+		Debug.Log( "Tap gesture detected at " + gesture.Position + 
+		          ". It was sent by " + gesture.Recognizer.name );
+	}
+
+
+	void OnSwipe( SwipeGesture gesture ) 
+	{
+		// Total swipe vector (from start to end position)
+		Vector2 move = gesture.Move;
+		
+		// Instant gesture velocity in screen units per second
+		float velocity = gesture.Velocity;
+		
+		// Approximate swipe direction
+		FingerGestures.SwipeDirection direction = gesture.Direction;
+
+		UI.StatusText = "Swiped " + direction + " with finger " + gesture.Fingers[0] +
+			" (velocity:" + velocity + ", distance: " + gesture.Move.magnitude + " )";
+
+		Debug.Log (UI.StatusText);
+	}
+}
---
+++ gestureApplication/Assets/gestureUI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gestureUI : MonoBehaviour {
+		public GUISkin skin;
+		GUIStyle statusStyle;
+		Rect statusTextRect = new Rect( 30, 336, 540, 60 );
+		string statusText = "";//"status text goes here";
+		public string StatusText
+		{
+			get { return statusText; }
+			set { statusText = value; }
+		}
+		
+		public bool showStatusText = true;
+		
+		void Awake()
+		{
+			statusStyle = new GUIStyle( skin.label );
+			statusStyle.alignment = TextAnchor.LowerCenter;
+		}
+		
+		#region Virtual Screen for automatic UI resolution scaling
+		
+		public static readonly float VirtualScreenWidth = 600;
+		public static readonly float VirtualScreenHeight = 400;
+		
+		public static void ApplyVirtualScreen()
+		{
+			// resolution scaling matrix
+			GUI.matrix = Matrix4x4.Scale( new Vector3( Screen.width / VirtualScreenWidth, Screen.height / VirtualScreenHeight, 1 ) );
+		}
+		
+		#endregion
+		
+		protected virtual void OnGUI()
+		{
+			if( skin != null )
+				GUI.skin = skin;
+			
+			ApplyVirtualScreen();
+			
+			if( showStatusText )
+				GUI.Label( statusTextRect, statusText, statusStyle );
+		}
+	}
